d4376b61740b341b898ed8ee03951fede1ec86b4
zisov <erelziso@gmail.com>
2020-09-26 18:21:56 +0300

    trying something

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -42,7 +42,6 @@ import static android.Manifest.permission.READ_EXTERNAL_STORAGE;
 import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-
     private final int PERMISSION_REQUEST_CODE = 200 ;
     private ImageView logOutPhoto;
     private TextView helloTextView;
